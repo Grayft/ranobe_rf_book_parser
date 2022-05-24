d90ca4ba1f8e6e680d4f45686bdae9844bfbeac9
Grayft <vas12332@mail.ru>
2022-05-24 23:31:22 +0300

    Was taken chapters json-data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
-from json import load
+from bs4 import BeautifulSoup
+from json import loads
 
 # 'https://ранобэ.рф'
 # URL = 'https://xn--80ac9aeh6f.xn--p1ai/'
@@ -9,12 +10,32 @@ PARAMS = []
 
 
 def get_site_content(url):
-    request = requests.get(url=url)
-    if request.status_code == 200:
+    response = requests.get(url=url)
+    if response.status_code == 200:
         print('Connection: OK')
-        print(request.json())
         # with open("site-page.html", 'w', encoding='utf-8') as file:
-        #     file.write(request.json)
+        #     file.write(response.text)
+        soup = BeautifulSoup(response.text, features='html.parser')
+        # with open('parsing.txt', 'w') as p_file:
+        data = soup.find('script', attrs={'id': '__NEXT_DATA__'})
+        s = str(data)
+        s = s.replace('\u200b', '')
+        s = s.replace('\\u003c', '<')
+        s = s.replace('\\u003e', '>')
+        s = s[51:-9]
+        j = loads(s)
+        b = j['props']
+        with open('chapters.txt', 'w') as chapter_file:
+
+            for i in j['props']['pageProps']['book']['chapters'][:5]:
+                chapter_file.write(str(i))
+                chapter_file.write('\n')
+            # print(str(i))
+            # p_file.write(s)
+            # for i, d in enumerate(data):
+
+            # print(l)
+        # print(soup.find_all('script', attrs={'id': '__NEXT_DATA__'}))
 
 
 def main():
